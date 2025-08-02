97a1d19c1ec2d4299fd862de3470327eeadee69a
sandeep <sandeepsanu1230@gmail.com>
2025-08-02 12:03:49 +0530

    updated api and authstore

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "@/stores/authStore";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+  process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- client/src/stores/authStore.js
+++ client/src/stores/authStore.js
@@ -4,7 +4,7 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+  process.env.NEXT_PUBLIC_API_URL;
 
 export const useAuthStore = create(
   persist(
