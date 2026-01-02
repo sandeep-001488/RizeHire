acc42e467f365c582f9c0996b60a158eac305747
sandeep-001488 <sandeep.kumar@clear.in>
2026-01-02 19:11:43 +0530

    removed unnecessary logs

--- client/src/app/jobs/[jobId]/page.jsx
+++ client/src/app/jobs/[jobId]/page.jsx
@@ -234,7 +234,7 @@ export default function JobDetailPage({ params }) {
 
   if (isLoading) {
     return (
-      <div className="flex items-center justify-center min-h-[400px]">
+      <div className="flex items-center justify-center min-h-400px">
         <Loader2 className="h-8 w-8 animate-spin" />
       </div>
     );
